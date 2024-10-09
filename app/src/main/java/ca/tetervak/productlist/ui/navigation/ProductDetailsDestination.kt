3e1c1f117bfb891abc5fc27514e3c6d44452f3d8
Alex Tetervak <tetervak@sheridancollege.ca>
2024-10-08 23:36:07 -0400

    updated build files

--- app/src/main/java/ca/tetervak/productlist/ui/navigation/ProductDetailsDestination.kt
+++ app/src/main/java/ca/tetervak/productlist/ui/navigation/ProductDetailsDestination.kt
@@ -3,7 +3,7 @@ package ca.tetervak.productlist.ui.navigation
 import ca.tetervak.productlist.R
 
 object ProductDetailsDestination : NavigationDestination {
-    override val route = "product_details"
+    override val route = "product-details"
     override val titleRes = R.string.product_details_title
     const val PRODUCT_ID_ARG = "productId"
     val routeWithArgs = "$route/{$PRODUCT_ID_ARG}"
